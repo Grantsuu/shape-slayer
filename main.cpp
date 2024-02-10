fa18b6092084b5688c0221ed36f2af1cb9217c3f
Grant Sherman <tnarg122@gmail.com>
2024-02-10 03:02:56 -0500

    rudamentary start menu

--- main.cpp
+++ main.cpp
@@ -5,11 +5,19 @@
 
 int main()
 {
+    enum GameState
+    {
+        kStartMenu,
+        kGameModeCountdown,
+        kGameModeCountdownEnd,
+        num_values
+    };
+
     // Game constants
     // --------------------------------------------------------------------
     const int kScreenWidth = 1200;
     const int kScreenHeight = 800;
-    const char* kWindowTitle = "Shape Slayer";
+    const char *kWindowTitle = "Shape Slayer";
     const int kInfoAreaX = 0;
     const int kInfoAreaY = 100;
     const int kFPS = 60;
@@ -20,6 +28,7 @@ int main()
 
     // Game variables
     // --------------------------------------------------------------------
+    GameState state = kStartMenu;
     ShapeManager sm = ShapeManager(kInfoAreaX, kInfoAreaY, kScreenWidth, kScreenHeight);
     Shape *shape = sm.GenerateRandomShape();
     int score = 0;
@@ -38,7 +47,7 @@ int main()
     {
         // Exit window
         // --------------------------------------------------------------------
-        if (WindowShouldClose() || IsKeyPressed(KEY_SPACE))
+        if (WindowShouldClose())
         {
             exitWindow = true;
         }
@@ -60,43 +69,105 @@ int main()
 
         ClearBackground(BLACK);
 
-        if (time < 1)
+        // Game screens to draw
+        // --------------------------------------------------------------------
+        switch (state)
         {
-            DrawText("Game Over!", kScreenWidth / 2 - 100, kScreenHeight / 2 - 50, 80, WHITE);
-            DrawText(TextFormat("Final Score: %i", score), kScreenWidth / 2 - 100, kScreenHeight / 2 + 50, 60, WHITE);
-            EndDrawing();
-            continue;
-        }
-        else
-        {
-            if (shape != nullptr)
-            {
-                sm.DrawShape(shape);
-            }
+        case kStartMenu: {
+            // Start menu constants
+            // --------------------------------------------------------------------
+            const char *kTitleText = "Welcome to Shape Slayer!";
+            const char *kCountdownText = "Play Countdown";
+            const char *kExitText = "Exit";
+            const int kTitleSize = 80;
+            const int kTitleMenuSize = 60;
+            const Rectangle kCountdownButton = {
+                static_cast<float>(kScreenWidth / 2 - MeasureText(kCountdownText, kTitleMenuSize) / 2),
+                static_cast<float>(kScreenHeight / 2 - 50),
+                static_cast<float>(MeasureText(kCountdownText, kTitleMenuSize)), static_cast<float>(kTitleMenuSize)};
+            const Rectangle kExitButton = {
+                static_cast<float>(kScreenWidth / 2 - MeasureText(kExitText, kTitleMenuSize) / 2),
+                static_cast<float>(kScreenHeight / 2 + 50), static_cast<float>(MeasureText(kExitText, kTitleMenuSize)),
+                static_cast<float>(kTitleMenuSize)};
+            // --------------------------------------------------------------------
 
+            // Draw start menu
+            // --------------------------------------------------------------------
+            DrawText(kTitleText, kScreenWidth / 2 - MeasureText(kTitleText, kTitleSize) / 2, kScreenHeight / 2 - 200,
+                     kTitleSize, BEIGE);
+            DrawText(kCountdownText, kCountdownButton.x, kCountdownButton.y, kTitleMenuSize, WHITE);
+            DrawText(kExitText, kExitButton.x, kExitButton.y, kTitleMenuSize, WHITE);
+            // --------------------------------------------------------------------
+
+            // Start menu input
+            // --------------------------------------------------------------------
             if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
             {
-                if (sm.CheckCollisionShape(shape, {static_cast<float>(GetMouseX()), static_cast<float>(GetMouseY())}))
+                // Countdown button
+                // --------------------------------------------------------------------
+                if (CheckCollisionPointRec({static_cast<float>(GetMouseX()), static_cast<float>(GetMouseY())},
+                                           kCountdownButton))
                 {
-                    score++;
-                    delete shape;
-                    shape = sm.GenerateRandomShape();
+                    state = kGameModeCountdown;
                 }
-                else
-                {
-                    score--;
-                }
-            }
+                // --------------------------------------------------------------------
 
-            // Information display
+                // Exit button
+                // --------------------------------------------------------------------
+                if (CheckCollisionPointRec({static_cast<float>(GetMouseX()), static_cast<float>(GetMouseY())},
+                                           kExitButton))
+                {
+                    exitWindow = true;
+                }
+                // --------------------------------------------------------------------
+            }
             // --------------------------------------------------------------------
-            DrawRectangleRec({0, 0, kScreenWidth, kInfoAreaY}, WHITE);
-            Color scoreColor = score >= kScoreColor ? GREEN : BLACK;
-            DrawText(TextFormat("Score: %i", score), 10, 10, 80, scoreColor);
-            Color timeColor = time <= kWarningTime ? RED : BLACK;
-            DrawText(TextFormat("Time: %i", time), 850, 10, 80, timeColor);
-            // --------------------------------------------------------------------
+            break;
         }
+        case kGameModeCountdown: {
+            if (time < 1)
+            {
+                DrawText("Game Over!", kScreenWidth / 2 - 100, kScreenHeight / 2 - 50, 80, WHITE);
+                DrawText(TextFormat("Final Score: %i", score), kScreenWidth / 2 - 100, kScreenHeight / 2 + 50, 60,
+                         WHITE);
+                EndDrawing();
+                continue;
+            }
+            else
+            {
+                if (shape != nullptr)
+                {
+                    sm.DrawShape(shape);
+                }
+
+                if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+                {
+                    if (sm.CheckCollisionShape(shape,
+                                               {static_cast<float>(GetMouseX()), static_cast<float>(GetMouseY())}))
+                    {
+                        score++;
+                        delete shape;
+                        shape = sm.GenerateRandomShape();
+                    }
+                    else
+                    {
+                        score--;
+                    }
+                }
+
+                // Information display
+                // --------------------------------------------------------------------
+                DrawRectangleRec({0, 0, kScreenWidth, kInfoAreaY}, WHITE);
+                Color scoreColor = score >= kScoreColor ? GREEN : BLACK;
+                DrawText(TextFormat("Score: %i", score), 10, 10, 80, scoreColor);
+                Color timeColor = time <= kWarningTime ? RED : BLACK;
+                DrawText(TextFormat("Time: %i", time), 850, 10, 80, timeColor);
+                // --------------------------------------------------------------------
+            }
+            break;
+        }
+        }
+        // --------------------------------------------------------------------
 
         EndDrawing();
         // ####################################################################
